8231c48a9a8a312a4bca26bb8d247bfc299639b3
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-13 20:29:53 -0600

    Add basic form structure

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Form.css';
+
+class Form extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <p>form!</p>
+    )
+  }
+};
+
+export default Form;
