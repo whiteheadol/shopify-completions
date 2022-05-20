5cc26386861c66f39b9c6ca6f307fea177cfff0a
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-19 20:38:47 -0600

    Restructure form to change the app's state upon a successful post response

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,46 +6,77 @@ class Form extends Component {
     super();
     this.state = {
       prompt: '',
-      response: '',
+      error: false,
+      input: true
     }
   }
 
   handleChange = event => {
-    this.setState({[event.target.name]: event.target.value})
+    this.setState({prompt: event.target.value})
   }
 
-  submitIdea = event => {
-    event.preventDefault();
-    // this.props.addIdea(newPrompt)
-    // invoke method to post, returning new resposne?
-    this.postPrompt();
+  createNewCard = (response) => {
+    const newCard = {
+      id: Date.now(),
+      prompt: this.state.prompt,
+      response: response,
+    }
+    this.props.addCard(newCard);
     this.clearInput();
   }
 
-  postPrompt = () => {
+  submitIdea = (event) => {
+    event.preventDefault();
+    if (this.state.prompt) {
+      this.postPrompt(this.state.prompt);
+      this.setState({input: true})
+    } else {
+      this.setState({input: false})
+    }
+  }
+
+  postPrompt = (input) => {
     const data = {
-      prompt: this.state.prompt,
+      prompt: `hot, hiking, inexpensive: You should visit Ecuador!
+      museums, shopping, fashion: You should visit Milan, Italy!
+      ocean, relaxing, hot: You should visit Cancun, Mexico!
+      ${input}:`,
       temperature: 0.6,
       max_tokens: 50,
       top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0
+      frequency_penalty: 0.4,
+      presence_penalty: 0.4
     }
+
+    let key = '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';
+    // let key2;
 
     fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+      Authorization: `Bearer ${key}`,
     },
       body: JSON.stringify(data),
     })
-    .then(resonse => console.log(response))
-  }
+    .then(response => {
+      if (response.ok) {
+        this.setState({ error: false });
+        return response.json()
+      } else {
+        throw Error(response.statusText)
+      }
+    })
+    .then(data => data.choices[0].text)
+    .then((text) => this.createNewCard(text))
+    .catch(err => {
+      this.setState({ error: true })
+    })
+  };
 
   clearInput = () => {
-    this.setState({ prompt: '', response: '' })
-  }
+    this.setState({ prompt: '',  })
+  };
 
   render() {
     return (
@@ -58,8 +89,11 @@ class Form extends Component {
           className="form-input"
           value={this.state.prompt}
           onChange={event => this.handleChange(event)}
+          required
         />
         <button className="submit-button" onClick={event => this.submitIdea(event)}>let's go</button>
+        {this.state.error && <h3 className="error-message">oh no, something went wrong! please try again soon</h3>}
+        {!this.state.input && <h3 className="input-error-message">please provide a valid input</h3>}
       </form>
     )
   }
