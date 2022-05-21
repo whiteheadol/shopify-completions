785645c166d8478d4efdb6cc9a37bd9aa899ea97
Olivia Whitehead <oliviawhitehead3@gmail.com>
2022-05-20 20:52:16 -0600

    Move ideasContainers files

---
+++ src/components/IdeasContainer/IdeasContainer.js
@@ -0,0 +1,25 @@
+import React, {Fragment} from 'react';
+import Idea from '../Idea/Idea';
+import './IdeasContainer.css';
+
+
+const IdeasContainer = ({ ideas }) => {
+  const ideaComponents = ideas.map(el => {
+    return (
+      <Idea
+        prompt={el.prompt}
+        response={el.response}
+        key={el.id}
+      />
+    )
+  })
+
+  return (
+    <div className="ideas-container">
+      <h2 className="ideas-title">your past inspiration:</h2>
+      {ideaComponents}
+    </div>
+  )
+};
+
+export default IdeasContainer;
